f9b01db87188581d80b5bd54a373a343327d80f5
5hanth <sh4nth@gmail.com>
2013-08-13 20:09:41 +0530

    fb working

--- upload.rb
+++ upload.rb
@@ -1,41 +1,49 @@
 #!/usr/bin/env ruby
+
 config = Dir.home+'/.access'
+
 if File.file? config
-load config
+	load config
 else
-puts "Configuration File #{config} not found !"
-exit
+	puts "Configuration File #{config} not found !"
+	exit
 end
+
 require 'fb_graph'
 
 class Uploader
 
-	def fb
+	def upload_to_fb
 
-		print "Photos : "
-		Dir["*"].each do |file|
-		print file + "\t"
-		end
-
-		puts "\nAre you sure you want to upload these photos to Facebook ? <y,n>"
+		puts "\nPhotos : "
+		system 'ls'	
+		print "\nAre you sure you want to upload these photos to Facebook ? <y,n> "
 		c = gets.chomp
 
 		if c.upcase == 'Y'  
 
-			Dir["*"].each do |file| 
-			a.photo!( 
-					:access_token=>ACCESS_TOKEN, 
-					:source=>File.new( file ), 
-					:message=>File.basename( file, File.extname(file) )
-				)
-			end
-		else exit
+				me = FbGraph::User.me(ACCESS_TOKEN)
+				album_name = File.basename(Dir.pwd)
+				puts "Creating new album '#{album_name}' ..."
+				album = me.album!( :name => album_name )
+
+
+				Dir["*"].each do |file| 
+					puts "Uploading #{file} ..... "
+					album.photo!( 
+							:access_token=>ACCESS_TOKEN, 
+							:source=>File.new( file ), 
+							:message=>File.basename( file, File.extname(file) )
+						    )
+				end
+			else exit
 		end
 
+		puts "Photos Successfully uploaded to Facebook."
 	end
 end
 
 # ------- Main -------
 
-bot = Uploader.new
-bot.fb
+	lazy_guy = Uploader.new
+	lazy_guy.upload_to_fb
