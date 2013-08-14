31e2c30bc57b0abe302c8f6c1e26ebbebd6eedbd
5hanth <sh4nth@gmail.com>
2013-08-14 19:57:03 +0530

    new file:   .access 	new file:   README

--- upload.rb
+++ upload.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 
-config = Dir.home+'/.access'
+# https://github.com/5hanth/lazyuploader
+# mail@shanth.tk
+
+config = Dir.home+'/.access'	# change your config file name accordingly.
 
 if File.file? config
 	load config
@@ -13,28 +16,37 @@ require 'fb_graph'
 
 class Uploader
 
+	def initialize
+
+		puts "\nPhotos :\n "
+		system 'ls'	
+
+	end
+
 	def upload_to_fb
 
-		puts "\nPhotos : "
-		system 'ls'	
 		print "\nAre you sure you want to upload these photos to Facebook ? <y,n> "
-		c = gets.chomp
 
-		if c.upcase == 'Y'  
+		if gets.chomp.upcase == 'Y'  
 
 				me = FbGraph::User.me(FB_ACCESS_TOKEN)
 				album_name = File.basename(Dir.pwd)
 				puts "Creating new album '#{album_name}' ..."
 				album = me.album!( :name => album_name )
 
+				photos = Dir["*"].map { |blah| blah if File.file? blah } #  get array of file names.
+				photos.delete nil
 
-				Dir["*"].each do |file| 
-					puts "Uploading #{file} ..... "
+				count = 0
+				total = photos.count
+
+				photos.each do |file| 
+					puts "Uploading #{file} ..... ( #{count = count.next} of #{total} )"
 					album.photo!( 
 							:access_token=>FB_ACCESS_TOKEN, 
 							:source=>File.new( file ), 
 							:message=>File.basename( file, File.extname(file) )
-						    )
+						    ) # remove :message if description should not be added to photos.
 				end
 			else exit
 		end
